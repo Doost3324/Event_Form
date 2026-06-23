f4b18ff4c19e5f12f53f98b29d5876b3380ae724
Doost3324 <saputskiyr@gmail.com>
2026-06-23 17:01:01 +0300

    js stuff + imgs + overall polish

--- js/script.js
+++ js/script.js
@@ -32,3 +32,61 @@ document.querySelectorAll(".social-dropdown")
         }
     });
 });
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+
+            entry.target.classList.add("show");
+        }
+
+    });
+
+}, {
+    threshold: 0.15
+});
+
+document
+.querySelectorAll("section")
+.forEach(section => {
+
+    section.classList.add("fade-in");
+
+    observer.observe(section);
+});
+
+// const sections =
+//     document.querySelectorAll("section");
+
+// const navLinks =
+//     document.querySelectorAll("nav a");
+
+// window.addEventListener("scroll", () => {
+
+//     let current = "";
+
+//     sections.forEach(section => {
+
+//         const top = section.offsetTop - 200;
+
+//         if (scrollY >= top) {
+
+//             current = section.id;
+//         }
+//     });
+
+//     navLinks.forEach(link => {
+
+//         link.classList.remove("active");
+
+//         if (
+//             link.getAttribute("href")
+//             === "#" + current
+//         ) {
+
+//             link.classList.add("active");
+//         }
+//     });
+// });
